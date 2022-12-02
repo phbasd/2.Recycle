1a36754e53fb79055725d53ef10d547ad662a18d
MINJU KIM <yd5726@naver.com>
2022-12-02 12:55:51 +0900

    update 3
    update 3

--- And11_AllView/app/src/main/java/com/example/and11_allview/human/HumanAdapter.java
+++ And11_AllView/app/src/main/java/com/example/and11_allview/human/HumanAdapter.java
@@ -10,16 +10,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.and11_allview.DetailActivity;
-import com.example.and11_allview.MainActivity;
 import com.example.and11_allview.R;
+
+import java.util.ArrayList;
 
 //2.
 public class HumanAdapter extends RecyclerView.Adapter<HumanAdapter.HumanViewHolder>{
     LayoutInflater inflater;
+    ArrayList<HumanDTO> list;
 
     public HumanAdapter(LayoutInflater inflater) {
         this.inflater = inflater;
+    }
+
+    public HumanAdapter(LayoutInflater inflater, ArrayList<HumanDTO> list) {
+        this.inflater = inflater;
+        this.list = list;
     }
 
     //3.
@@ -34,9 +40,9 @@ public class HumanAdapter extends RecyclerView.Adapter<HumanAdapter.HumanViewHol
 
     @Override
     public void onBindViewHolder(@NonNull HumanViewHolder h, int i) {
-        h.human_title.setText("사과");
+        /*h.human_title.setText("사과");
         h.human_txt.setText("맛있는 밀양 얼음골 사과 팝니다.");
-        h.human_price.setText("50,000원");
+        h.human_price.setText("50,000원");*/
         h.human_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +50,21 @@ public class HumanAdapter extends RecyclerView.Adapter<HumanAdapter.HumanViewHol
                 v.getContext().startActivity(intent);
             }
         });
+
+        ImageView human_img = h.itemView.findViewById(R.id.human_img);
+        TextView human_title = h.human_title.findViewById(R.id.human_title);
+        TextView human_txt = h.human_txt.findViewById(R.id.human_txt);
+        TextView human_price = h.human_price.findViewById(R.id.human_price);
+
+        HumanDTO humanDTO = list.get(i);
+        int resId = humanDTO.getPoster();
+        human_img.setImageResource(resId);
+        String title = humanDTO.getHuman_title();
+        String txt = humanDTO.getHuman_txt();
+        String price = humanDTO.getHuman_price();
+        human_title.setText(title);
+        human_txt.setText(txt);
+        human_price.setText(price);
     }
 
     //4.
